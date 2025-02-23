c89d85cb8549665ba7d8575778a3147f1911df8b
ANDI <andi.amine@gmail.com>
2025-02-23 13:49:44 +0000

    Improved - Navigatin bar UI/UX improved

--- components/com_joomsubscription/layouts/links.php
+++ components/com_joomsubscription/layouts/links.php
@@ -4,67 +4,49 @@
  * a component for Joomla! 1.7 - 2.5 CMS (http://www.joomla.org)
  * Author Website: https://www.joomcoder.com/
  * @copyright Copyright (C) 2012 JoomCoder (https://www.joomcoder.com/). All rights reserved.
- * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ * @license   GNU/GPL http://www.gnu.org/copyleft/gpl.html
  */
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Router\Route;
 
 defined('_JEXEC') or die();
 
-if(!JoomsubscriptionHelper::isModer()) return;
+if (!JoomsubscriptionHelper::isModer()) return;
 $view = JFactory::getApplication()->input->getCmd('view');
-$img_url =  JUri::root(TRUE).'/components/com_joomsubscription/images/cpanel/';
+
+$navMenu = [
+	['title' => 'ESUBSCRIPTIONS', 'url' => 'index.php?option=com_joomsubscription&view=emsales'],
+	['title' => 'EPLANS', 'url' => 'index.php?option=com_joomsubscription&view=emplans'],
+	['title' => 'EGROUPS', 'url' => 'index.php?option=com_joomsubscription&view=emgroups'],
+	['title' => 'EFIELDS', 'url' => 'index.php?option=com_joomsubscription&view=emfields'],
+	['title' => 'ECOUPONS', 'url' => 'index.php?option=com_joomsubscription&view=emcoupons'],
+	['title' => 'EANALYTICS', 'url' => 'index.php?option=com_joomsubscription&view=emanalytics'],
+	['title' => 'ESTATES', 'url' => 'index.php?option=com_joomsubscription&view=emstates'],
+	['title' => 'ETAXES', 'url' => 'index.php?option=com_joomsubscription&view=emtaxes'],
+	['title' => 'EPLUNLIST', 'url' => 'index.php?option=com_joomsubscription&view=emlist'],
+	['title' => 'EPLUNHISTORY', 'url' => 'index.php?option=com_joomsubscription&view=emhistory'],
+];
+
+HTMLHelper::_('bootstrap.collapse', '.navbar-toggler');
+
 ?>
-<style>
-.joomsubscription a.brand,
-.joomsubscription li {line-height: 27px;}
-.joomsubscription a.brand img{width: 30px;}
-</style>
-<div class="navbar joomsubscription">
-	<div class="navbar-inner">
-		<a class="brand" href="<?php echo JRoute::_('index.php?option=com_joomsubscription&view=emcpanel')?>"><img src="<?php echo $img_url; ?>dashboard-small.png" align="absmiddle"/> <strong>Joomsubscription <sup><small>10</small></sup></strong></a>
-		<ul class="nav">
-		<li class="<?php echo($view == 'emsales' ? 'active' : NULL); ?>">
-			<a href="<?php echo JRoute::_('index.php?option=com_joomsubscription&view=emsales') ?>">
-				<img src="<?php echo $img_url; ?>sales-small.png" align="absmiddle"/> <?php echo JText::_('ESUBSCRIPTIONS') ?></a></li>
-		<li class="<?php echo($view == 'emplans' ? 'active' : NULL); ?>">
-			<a href="<?php echo JRoute::_('index.php?option=com_joomsubscription&view=emplans') ?>">
-				<img src="<?php echo $img_url; ?>plans-small.png" align="absmiddle"/> <?php echo JText::_('EPLANS') ?></a></li>
-		<li class="<?php echo($view == 'emgroups' ? 'active' : NULL); ?>">
-			<a href="<?php echo JRoute::_('index.php?option=com_joomsubscription&view=emgroups') ?>">
-				<img src="<?php echo $img_url; ?>groups-small.png" align="absmiddle"/> <?php echo JText::_('EGROUPS') ?></a></li>
-		<li class="<?php echo($view == 'emfields' ? 'active' : NULL); ?>">
-			<a href="<?php echo JRoute::_('index.php?option=com_joomsubscription&view=emfields') ?>">
-				<img src="<?php echo $img_url; ?>fields-small.png" align="absmiddle"/> <?php echo JText::_('EFIELDS') ?></a></li>
-
-			<li class="dropdown <?php echo(in_array($view, array('emcoupons', 'emanalytics', 'emstates', 'emtaxes', 'emimports', 'emlist', 'emhistory')) ? 'active' : NULL); ?>">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <img src="<?php echo $img_url; ?>more-small.png" align="absmiddle"/> <?php echo JText::_('EOTHER')?> <i class="caret"></i>
-				</a>
-				<ul class="dropdown-menu">
-					<li class="<?php echo($view == 'emcoupons' ? 'uk-active' : NULL); ?>">
-						<a href="<?php echo JRoute::_('index.php?option=com_joomsubscription&view=emcoupons') ?>">
-							<img src="<?php echo $img_url; ?>coupons-small.png" align="absmiddle"/> <?php echo JText::_('ECOUPONS') ?></a></li>
-					<li class="<?php echo($view == 'emanalytics' ? 'uk-active' : NULL); ?>">
-						<a href="<?php echo JRoute::_('index.php?option=com_joomsubscription&view=emanalytics') ?>">
-							<img src="<?php echo $img_url; ?>analytics-small.png" align="absmiddle"/> <?php echo JText::_('EANALYTICS') ?></a></li>
-					<li class="<?php echo($view == 'emstates' ? 'uk-active' : NULL); ?>">
-						<a href="<?php echo JRoute::_('index.php?option=com_joomsubscription&view=emstates') ?>">
-							<img src="<?php echo $img_url; ?>states-small.png" align="absmiddle"/> <?php echo JText::_('ESTATES') ?></a></li>
-					<li class="<?php echo($view == 'emtaxes' ? 'uk-active' : NULL); ?>">
-						<a href="<?php echo JRoute::_('index.php?option=com_joomsubscription&view=emtaxes') ?>">
-							<img src="<?php echo $img_url; ?>taxes-small.png" align="absmiddle"/> <?php echo JText::_('ETAXES') ?></a></li>
-					<li class="<?php echo($view == 'emimports' ? 'uk-active' : NULL); ?>">
-						<a href="<?php echo JRoute::_('index.php?option=com_joomsubscription&view=emimports') ?>">
-							<img src="<?php echo $img_url; ?>import-small.png" align="absmiddle"/> <?php echo JText::_('EIMPORT') ?></a></li>
-					<li class="divider"></li>
-					<li class="<?php echo($view == 'emlist' ? 'uk-active' : NULL); ?>">
-						<a href="<?php echo JRoute::_('index.php?option=com_joomsubscription&view=emlist') ?>">
-							<img src="<?php echo $img_url; ?>purchase-small.png" align="absmiddle"/> <?php echo JText::_('EPLUNLIST') ?></a></li>
-					<li class="<?php echo($view == 'emhistory' ? 'uk-active' : NULL); ?>">
-						<a href="<?php echo JRoute::_('index.php?option=com_joomsubscription&view=emhistory') ?>">
-							<img src="<?php echo $img_url; ?>history-small.png" align="absmiddle"/> <?php echo JText::_('EPLUNHISTORY') ?></a></li>
-				</ul>
-
-			</li>
-		</ul>
-	</div>
-</div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary mb-3 rounded shadow-sm border">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <?php foreach ($navMenu as $menuItem): ?>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="<?php echo Route::_($menuItem['url'])  ?>">
+                            <?php echo \Joomla\CMS\Language\Text::_($menuItem['title']) ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
+</nav>
